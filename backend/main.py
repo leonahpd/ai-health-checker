a8ea1012ebdfa24e21190c88914727361e5e57f7
Leona Hpd <lionaraj97@gmail.com>
2026-03-27 21:54:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For development
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(predict.router)
+
+@app.get("/")
+def root():
+    return {"message": "Backend running in Kubernetes 🚀"}
