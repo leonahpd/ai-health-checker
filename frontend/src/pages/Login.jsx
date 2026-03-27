c1b2a0a29064c51ba2bdca1dc03a9dc3b4df71b4
Leona Hpd <lionaraj97@gmail.com>
2026-03-27 21:06:43 +0530

    Add Login component for user authentication

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
