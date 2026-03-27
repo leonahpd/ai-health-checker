6247fab960f247c7ae174e219f3ea2a80503d1de
Leona Hpd <lionaraj97@gmail.com>
2026-03-27 21:16:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "http://127.0.0.1:8000",
 });
 
 export const predictDisease = (symptoms) => {
-  return API.post("/predict", symptoms);
-};;
+  return API.post("/predict", {
+    symptoms: symptoms
+  });
+};
